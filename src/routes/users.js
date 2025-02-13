76d7dc4ae0dc3851d179f34c8aee2f66522d21db
Marco <marco.dmail96@gmail.com>
2025-02-13 12:01:59 +0100

    Update users route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getUsers from "../services/users/getUsers.js";
+import createUser from "../services/users/createUser.js";
 import auth from "../middleware/auth.js";
 
 const router = Router();
@@ -8,6 +9,24 @@ router.get("/", async (req, res, next) => {
   try {
     const users = await getUsers();
     res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+    const newUser = await createUser(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture
+    );
+    res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
