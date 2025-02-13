89031064c75248229cd2efc4fed34906b5a2a4e6
Marco <marco.dmail96@gmail.com>
2025-02-13 12:44:22 +0100

    Update users router

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import getUsers from "../services/users/getUsers.js";
 import createUser from "../services/users/createUser.js";
+import getUserById from "../services/users/getUserById.js";
 import auth from "../middleware/auth.js";
+import updateUserById from "../services/users/updateUserById.js";
 
 const router = Router();
 
@@ -27,6 +29,48 @@ router.post("/", async (req, res, next) => {
       profilePicture
     );
     res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
+    if (!user) {
+      res.status(404).json({ message: `User with ${id} not found!` });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+    const { id } = req.params;
+    const user = await updateUserById(id, {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+    });
+
+    if (user) {
+      res
+        .status(200)
+        .send({ message: `User with id ${id} has been updated`, user });
+    } else {
+      res
+        .status(404)
+        .json({ message: `User with id ${id} has not been found` });
+    }
   } catch (error) {
     next(error);
   }
