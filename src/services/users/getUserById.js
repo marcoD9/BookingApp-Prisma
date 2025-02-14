d6a4d1d26b70bc62af131c252f097e76e5adcaf7
Marco <marco.dmail96@gmail.com>
2025-02-14 10:54:02 +0100

    Update getUserById.js, now does not retrieve user password

--- src/services/users/getUserById.js
+++ src/services/users/getUserById.js
@@ -4,6 +4,14 @@ const getUserById = async (id) => {
   const prisma = new PrismaClient();
   const user = await prisma.user.findUnique({
     where: { id },
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      profilePicture: true,
+    },
   });
   return user;
 };
