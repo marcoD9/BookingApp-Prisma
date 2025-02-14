08edb7b4d65d4bc14e33992abd079fd5981e5730
Marco <marco.dmail96@gmail.com>
2025-02-14 11:29:35 +0100

    update hosts router

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import getHosts from "../services/hosts/getHosts.js";
 import getHostById from "../services/hosts/getHostById.js";
+import createHost from "../services/hosts/createHost.js";
+import updateHostById from "../services/hosts/updateHostById.js";
 
 const router = Router();
 
@@ -23,6 +25,68 @@ router.get("/:id", async (req, res, next) => {
         .json({ message: `Host with id ${id} has not been found!` });
     } else {
       res.json(host);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const host = await createHost(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe
+    );
+    res.status(201).json(host);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const host = await updateHostById(id, {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    });
+
+    if (host) {
+      res
+        .status(200)
+        .send({ message: `Host with id ${id} has been updated`, host });
+    } else {
+      res
+        .status(404)
+        .json({ message: `Host with id ${id} has not been found` });
     }
   } catch (error) {
     next(error);
