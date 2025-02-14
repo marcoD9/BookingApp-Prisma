8d063f7016a6272a2556ede50a2254b9106270f2
Marco <marco.dmail96@gmail.com>
2025-02-14 10:58:24 +0100

    Update hosts router

---
+++ src/routes/hosts.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import getHosts from "../services/hosts/getHosts.js";
+import getHostById from "../services/hosts/getHostById.js";
+
+const router = Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const hosts = await getHosts();
+    res.json(hosts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const host = await getHostById(id);
+    if (!host) {
+      res
+        .status(404)
+        .json({ message: `Host with id ${id} has not been found!` });
+    } else {
+      res.json(host);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
