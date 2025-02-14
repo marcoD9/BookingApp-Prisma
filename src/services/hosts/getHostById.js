0716319a0cfa441fc1b1b7bce816051f9fbd7871
Marco <marco.dmail96@gmail.com>
2025-02-14 10:57:57 +0100

    Create getHostById.js

---
+++ src/services/hosts/getHostById.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const getHostById = async (id) => {
+  const prisma = new PrismaClient();
+  const host = await prisma.host.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      profilePicture: true,
+      aboutMe: true,
+    },
+  });
+  return host;
+};
+
+export default getHostById;
