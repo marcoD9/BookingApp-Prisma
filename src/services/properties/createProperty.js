0aa0d6cc9e82aa9ba3c98efc25083a6646fdf6fc
Marco <marco.dmail96@gmail.com>
2025-02-16 21:54:10 +0100

    Update createProperty.js

--- src/services/properties/createProperty.js
+++ src/services/properties/createProperty.js
@@ -8,39 +8,24 @@ const createProperty = async (
   bedroomCount,
   bathRoomCount,
   maxGuestCount,
-  hostId
+  hostId,
+  rating
 ) => {
   const prisma = new PrismaClient();
-
-  try {
-    const existingHost = await prisma.host.findUnique({
-      where: { id: hostId },
-    });
-
-    if (!existingHost) {
-      throw new Error(`Host with ID ${hostId} not found.`);
-    }
-
-    const property = await prisma.property.create({
-      data: {
-        title,
-        description,
-        location,
-        pricePerNight,
-        bedroomCount,
-        bathRoomCount,
-        maxGuestCount,
-        host: { connect: { id: hostId } }, // Connect to the host
-      },
-    });
-
-    return property;
-  } catch (error) {
-    console.error("Error creating property:", error);
-    throw error;
-  } finally {
-    await prisma.$disconnect();
-  }
+  const property = await prisma.property.create({
+    data: {
+      title,
+      description,
+      location,
+      pricePerNight,
+      bedroomCount,
+      bathRoomCount,
+      maxGuestCount,
+      host: { connect: { id: hostId } }, // Connect to the host
+      rating,
+    },
+  });
+  return property;
 };
 
 export default createProperty;
