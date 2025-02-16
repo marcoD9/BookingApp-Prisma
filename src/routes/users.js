f6d4cf72e37ff3ca6efaa384f59bfa0d7a9d8032
Marco <marco.dmail96@gmail.com>
2025-02-16 22:35:23 +0100

    Update users router, handle 404 on /:id and missing POST fields

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,8 @@ import getUserById from "../services/users/getUserById.js";
 import auth from "../middleware/auth.js";
 import updateUserById from "../services/users/updateUserById.js";
 import deleteUserById from "../services/users/deleteUserById.js";
+import validateFields from "../middleware/validateMiddleware.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = Router();
 
@@ -18,83 +20,109 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/", auth, async (req, res, next) => {
-  try {
-    const { username, password, name, email, phoneNumber, profilePicture } =
-      req.body;
-    const newUser = await createUser(
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture
-    );
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await getUserById(id);
-    if (!user) {
-      res.status(404).json({ message: `User with ${id} not found!` });
-    } else {
-      res.status(200).json(user);
+router.post(
+  "/",
+  auth,
+  validateFields([
+    "username",
+    "password",
+    "name",
+    "email",
+    "phoneNumber",
+    "profilePicture",
+  ]),
+  async (req, res, next) => {
+    try {
+      const { username, password, name, email, phoneNumber, profilePicture } =
+        req.body;
+      const newUser = await createUser(
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture
+      );
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-router.put("/:id", auth, async (req, res, next) => {
-  try {
-    const { username, password, name, email, phoneNumber, profilePicture } =
-      req.body;
-    const { id } = req.params;
-    const user = await updateUserById(id, {
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture,
-    });
-
-    if (user) {
-      res
-        .status(200)
-        .send({ message: `User with id ${id} has been updated`, user });
-    } else {
-      res
-        .status(404)
-        .json({ message: `User with id ${id} has not been found` });
+router.get(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await getUserById(id);
+      if (!user) {
+        res.status(404).json({ message: `User with ${id} not found!` });
+      } else {
+        res.status(200).json(user);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
 
-router.delete("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await deleteUserById(id);
-    if (user) {
-      res.status(200).send({
-        message: `User with id ${id} has been successfully deleted!`,
-        user,
+router.put(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { username, password, name, email, phoneNumber, profilePicture } =
+        req.body;
+      const { id } = req.params;
+      const user = await updateUserById(id, {
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture,
       });
-    } else {
-      res
-        .status(404)
-        .json({ message: `User with id ${id} has not been found!` });
+
+      if (user) {
+        res
+          .status(200)
+          .send({ message: `User with id ${id} has been updated`, user });
+      } else {
+        res
+          .status(404)
+          .json({ message: `User with id ${id} has not been found` });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
+
+router.delete(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await deleteUserById(id);
+      if (user) {
+        res.status(200).send({
+          message: `User with id ${id} has been successfully deleted!`,
+          user,
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: `User with id ${id} has not been found!` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 
 export default router;
