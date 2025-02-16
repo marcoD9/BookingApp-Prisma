ecfa31daaf48724ae54e8a0bee3f78ed196f1151
Marco <marco.dmail96@gmail.com>
2025-02-16 16:49:56 +0100

    Update getHosts.js, now retrieve listings datas

--- src/services/hosts/getHosts.js
+++ src/services/hosts/getHosts.js
@@ -2,25 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 const getHosts = async () => {
   const prisma = new PrismaClient();
-  try {
-    const hosts = await prisma.host.findMany({
-      select: {
-        id: true,
-        username: true,
-        name: true,
-        email: true,
-        phoneNumber: true,
-        profilePicture: true,
-        aboutMe: true,
-      },
-    });
-    return hosts;
-  } catch (error) {
-    console.error("Error fetching hosts:", error);
-    throw error;
-  } finally {
-    await prisma.$disconnect();
-  }
+  const hosts = await prisma.host.findMany({
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      profilePicture: true,
+      aboutMe: true,
+      listings: true,
+    },
+  });
+  return hosts;
 };
 
 export default getHosts;
