0425fb45a53f408646b1bf3134e4ab9dde785a6e
Marco <marco.dmail96@gmail.com>
2025-02-16 21:54:34 +0100

    Update deleteUserById.js

--- src/services/users/deleteUserById.js
+++ src/services/users/deleteUserById.js
@@ -2,7 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 const deleteUserById = async (id) => {
   const prisma = new PrismaClient();
-
+  //Delete the bookings
+  await prisma.booking.deleteMany({
+    where: {
+      userId: id,
+    },
+  });
+  //Delete the reviews
+  await prisma.review.deleteMany({
+    where: {
+      userId: id,
+    },
+  });
+  //Now delete user
   const user = await prisma.user.deleteMany({
     where: { id },
   });
