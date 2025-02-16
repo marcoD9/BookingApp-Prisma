1562a6af7a708550339899651059b119de74013c
Marco <marco.dmail96@gmail.com>
2025-02-16 21:54:20 +0100

    Update createReview.js

--- src/services/reviews/createReview.js
+++ src/services/reviews/createReview.js
@@ -2,21 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const createReview = async (userId, propertyId, rating, comment) => {
   const prisma = new PrismaClient();
-  // Check if userId and propertyId exist
-  const [user, property] = await Promise.all([
-    prisma.user.findUnique({ where: { id: userId } }),
-    prisma.property.findUnique({
-      where: { id: propertyId },
-    }),
-  ]);
-
-  if (!user) {
-    throw new Error(`User with ID ${userId} not found.`);
-  }
-
-  if (!property) {
-    throw new Error(`Property with ID ${propertyId} not found.`);
-  }
   const review = await prisma.review.create({
     data: {
       user: { connect: { id: userId } },
