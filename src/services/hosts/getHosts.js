c090f965e142785a1468b0923903f8a1cad97f0e
Marco <marco.dmail96@gmail.com>
2025-02-16 17:45:25 +0100

    Update getHosts.js to handle queries

--- src/services/hosts/getHosts.js
+++ src/services/hosts/getHosts.js
@@ -1,8 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const getHosts = async () => {
+const getHosts = async (name) => {
   const prisma = new PrismaClient();
   const hosts = await prisma.host.findMany({
+    where: {
+      name: name ? name : undefined,
+    },
     select: {
       id: true,
       username: true,
