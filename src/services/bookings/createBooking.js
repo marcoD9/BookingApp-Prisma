96df8979dbb9c7ed3fa14f15683be4b125319dbd
Marco <marco.dmail96@gmail.com>
2025-02-16 21:53:40 +0100

    Update createBooking.js

--- src/services/bookings/createBooking.js
+++ src/services/bookings/createBooking.js
@@ -5,55 +5,18 @@ const createBooking = async (
   propertyId,
   checkinDate,
   checkoutDate,
-  numberOfGuests
+  numberOfGuests,
+  totalPrice,
+  bookingStatus
 ) => {
   const prisma = new PrismaClient();
 
-  // 1. Check if userId and propertyId exist
-  const [user, property] = await Promise.all([
-    prisma.user.findUnique({ where: { id: userId } }),
-    prisma.property.findUnique({
-      where: { id: propertyId },
-      select: { pricePerNight: true },
-    }),
-  ]);
-
-  if (!user) {
-    throw new Error(`User with ID ${userId} not found.`);
-  }
-
-  if (!property) {
-    throw new Error(`Property with ID ${propertyId} not found.`);
-  }
-
-  // 2. Find the total price based on checkin and checkout dates
-  const checkin = new Date(checkinDate);
-  const checkout = new Date(checkoutDate);
-
-  if (isNaN(checkin) || isNaN(checkout)) {
-    throw new Error(
-      "Invalid checkin or checkout date format. Dates must be valid."
-    );
-  }
-
-  const numberOfNights =
-    (checkout.getTime() - checkin.getTime()) / (1000 * 60 * 60 * 24);
-  if (numberOfNights < 0) {
-    throw new Error("Checkout date cannot be before checkin date.");
-  }
-
-  const totalPrice = property.pricePerNight * numberOfNights;
-
-  // 3. Set bookingStatus on pending by default
-  const bookingStatus = "pending";
-
-  // 4. Return booking
   const booking = await prisma.booking.create({
     data: {
       user: { connect: { id: userId } }, // Connect the user
       property: { connect: { id: propertyId } }, // Connect the property
-      checkinDate: checkin,
-      checkoutDate: checkout,
+      checkinDate,
+      checkoutDate,
       numberOfGuests,
       totalPrice,
       bookingStatus,
