a8534d1941dd31594af72fbde30d0c0be721aba4
Marco <marco.dmail96@gmail.com>
2025-02-16 16:50:40 +0100

    Update getProperties.js, now retrieve amenities and reviews datas

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -2,15 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const getProperties = async () => {
   const prisma = new PrismaClient();
-  try {
-    const properties = await prisma.property.findMany();
-    return properties;
-  } catch (error) {
-    console.error("Error fetching properties:", error);
-    throw error;
-  } finally {
-    await prisma.$disconnect();
-  }
+
+  const properties = await prisma.property.findMany({
+    include: {
+      amenities: true, // Include related amenities
+      reviews: true, // Include related reviews
+    },
+  });
+  return properties;
 };
 
 export default getProperties;
