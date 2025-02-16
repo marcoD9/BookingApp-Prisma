15e97adf0809e63825c9774bfd420b727ebfc947
Marco <marco.dmail96@gmail.com>
2025-02-16 22:35:15 +0100

    Update reviews router, handle 404 on /:id and missing POST fields

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -5,6 +5,8 @@ import deleteReviewById from "../services/reviews/deleteReviewById.js";
 import createReview from "../services/reviews/createReview.js";
 import updateReviewById from "../services/reviews/updateReviewById.js";
 import auth from "../middleware/auth.js";
+import validateFields from "../middleware/validateMiddleware.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = Router();
 
@@ -17,75 +19,94 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const review = await getReviewById(id);
-    if (!review) {
-      res
-        .status(404)
-        .json({ message: `Review with id ${id} has not been found!` });
-    } else {
-      res.status(200).json(review);
+router.get(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const review = await getReviewById(id);
+      if (!review) {
+        res
+          .status(404)
+          .json({ message: `Review with id ${id} has not been found!` });
+      } else {
+        res.status(200).json(review);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
 
-router.delete("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const review = await deleteReviewById(id);
-    if (review) {
-      res.status(200).send({
-        message: `Review with id ${id} successfully deleted!`,
-        review,
+router.delete(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const review = await deleteReviewById(id);
+      if (review) {
+        res.status(200).send({
+          message: `Review with id ${id} successfully deleted!`,
+          review,
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Review with id ${id} has not been found!` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
+
+router.post(
+  "/",
+  auth,
+  validateFields(["userId", "propertyId", "rating", "comment"]),
+  async (req, res, next) => {
+    try {
+      const { userId, propertyId, rating, comment } = req.body;
+      const review = await createReview(userId, propertyId, rating, comment);
+      res.status(201).json(review);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { userId, propertyId, rating, comment } = req.body;
+
+      const review = await updateReviewById(id, {
+        userId,
+        propertyId,
+        rating,
+        comment,
       });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Review with id ${id} has not been found!` });
+      if (review) {
+        res.status(200).send({
+          message: `Review with id ${id} has been successfully updated!`,
+          review,
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Review with id ${id} has not been found!` });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/", auth, async (req, res, next) => {
-  try {
-    const { userId, propertyId, rating, comment } = req.body;
-    const review = await createReview(userId, propertyId, rating, comment);
-    res.status(201).json(review);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { userId, propertyId, rating, comment } = req.body;
-
-    const review = await updateReviewById(id, {
-      userId,
-      propertyId,
-      rating,
-      comment,
-    });
-    if (review) {
-      res.status(200).send({
-        message: `Review with id ${id} has been successfully updated!`,
-        review,
-      });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Review with id ${id} has not been found!` });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
 
 export default router;
