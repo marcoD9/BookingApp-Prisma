a0687ee758e3f4b50af88d3757607f02dd0f894b
Marco <marco.dmail96@gmail.com>
2025-02-16 22:35:06 +0100

    Update properties router, handle 404 on /:id and missing POST fields

--- src/routes/properties.js
+++ src/routes/properties.js
@@ -5,6 +5,8 @@ import createProperty from "../services/properties/createProperty.js";
 import updatePropertyById from "../services/properties/updatePropertyById.js";
 import deletePropertyById from "../services/properties/deletePropertyById.js";
 import auth from "../middleware/auth.js";
+import validateFields from "../middleware/validateMiddleware.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = Router();
 
@@ -27,110 +29,142 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const property = await getPropertyById(id);
-    if (!property) {
-      res
-        .status(404)
-        .json({ message: `Property with id ${id} has not been found!` });
-    } else {
-      res.status(200).json(property);
+router.get(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const property = await getPropertyById(id);
+      if (!property) {
+        res
+          .status(404)
+          .json({ message: `Property with id ${id} has not been found!` });
+      } else {
+        res.status(200).json(property);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
 
-router.post("/", auth, async (req, res, next) => {
-  try {
-    const {
-      title,
-      description,
-      location,
-      pricePerNight,
-      bedroomCount,
-      bathRoomCount,
-      maxGuestCount,
-      hostId,
-      rating,
-    } = req.body;
-    const newProperty = await createProperty(
-      title,
-      description,
-      location,
-      pricePerNight,
-      bedroomCount,
-      bathRoomCount,
-      maxGuestCount,
-      hostId,
-      rating
-    );
-    res.status(201).json(newProperty);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", auth, async (req, res, next) => {
-  try {
-    const {
-      title,
-      description,
-      location,
-      pricePerNight,
-      bedroomCount,
-      bathRoomCount,
-      maxGuestCount,
-      hostId,
-      rating,
-    } = req.body;
-    const { id } = req.params;
-
-    const property = await updatePropertyById(id, {
-      title,
-      description,
-      location,
-      pricePerNight,
-      bedroomCount,
-      bathRoomCount,
-      maxGuestCount,
-      hostId,
-      rating,
-    });
-
-    if (property) {
-      res
-        .status(200)
-        .send({ message: `Property with id ${id} has been updated`, property });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Property with id ${id} has not been found` });
+router.post(
+  "/",
+  auth,
+  validateFields([
+    "title",
+    "description",
+    "location",
+    "pricePerNight",
+    "bedroomCount",
+    "bathRoomCount",
+    "maxGuestCount",
+    "hostId",
+    "rating",
+  ]),
+  async (req, res, next) => {
+    try {
+      const {
+        title,
+        description,
+        location,
+        pricePerNight,
+        bedroomCount,
+        bathRoomCount,
+        maxGuestCount,
+        hostId,
+        rating,
+      } = req.body;
+      const newProperty = await createProperty(
+        title,
+        description,
+        location,
+        pricePerNight,
+        bedroomCount,
+        bathRoomCount,
+        maxGuestCount,
+        hostId,
+        rating
+      );
+      res.status(201).json(newProperty);
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-router.delete("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const property = await deletePropertyById(id);
-    if (property) {
-      res.status(200).send({
-        message: `Property with id ${id} successfully deleted!`,
-        property,
+router.put(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const {
+        title,
+        description,
+        location,
+        pricePerNight,
+        bedroomCount,
+        bathRoomCount,
+        maxGuestCount,
+        hostId,
+        rating,
+      } = req.body;
+      const { id } = req.params;
+
+      const property = await updatePropertyById(id, {
+        title,
+        description,
+        location,
+        pricePerNight,
+        bedroomCount,
+        bathRoomCount,
+        maxGuestCount,
+        hostId,
+        rating,
       });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Property with id ${id} has not been found!` });
+
+      if (property) {
+        res
+          .status(200)
+          .send({
+            message: `Property with id ${id} has been updated`,
+            property,
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Property with id ${id} has not been found` });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
+
+router.delete(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const property = await deletePropertyById(id);
+      if (property) {
+        res.status(200).send({
+          message: `Property with id ${id} successfully deleted!`,
+          property,
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Property with id ${id} has not been found!` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 
 export default router;
