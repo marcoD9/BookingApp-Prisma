ba70942f72cac7a9a8313aac0817b3b898d4e848
Marco <marco.dmail96@gmail.com>
2025-02-16 22:34:58 +0100

    Update hosts router, handle 404 on /:id and missing POST fields

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -5,6 +5,8 @@ import createHost from "../services/hosts/createHost.js";
 import updateHostById from "../services/hosts/updateHostById.js";
 import deleteHostById from "../services/hosts/deleteHostById.js";
 import auth from "../middleware/auth.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
+import validateFields from "../middleware/validateMiddleware.js";
 
 const router = Router();
 
@@ -18,101 +20,128 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const host = await getHostById(id);
-    if (!host) {
-      res
-        .status(404)
-        .json({ message: `Host with id ${id} has not been found!` });
-    } else {
-      res.json(host);
+router.get(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const host = await getHostById(id);
+      if (!host) {
+        res
+          .status(404)
+          .json({ message: `Host with id ${id} has not been found!` });
+      } else {
+        res.json(host);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
 
-router.post("/", auth, async (req, res, next) => {
-  try {
-    const {
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture,
-      aboutMe,
-    } = req.body;
-    const host = await createHost(
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture,
-      aboutMe
-    );
-    res.status(201).json(host);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const {
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture,
-      aboutMe,
-    } = req.body;
-    const host = await updateHostById(id, {
-      username,
-      password,
-      name,
-      email,
-      phoneNumber,
-      profilePicture,
-      aboutMe,
-    });
-
-    if (host) {
-      res
-        .status(200)
-        .send({ message: `Host with id ${id} has been updated`, host });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Host with id ${id} has not been found` });
+router.post(
+  "/",
+  auth,
+  validateFields([
+    "username",
+    "password",
+    "name",
+    "email",
+    "phoneNumber",
+    "profilePicture",
+    "aboutMe",
+  ]),
+  async (req, res, next) => {
+    try {
+      const {
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture,
+        aboutMe,
+      } = req.body;
+      const host = await createHost(
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture,
+        aboutMe
+      );
+      res.status(201).json(host);
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-router.delete("/:id", auth, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const host = await deleteHostById(id);
-    if (host) {
-      res.status(200).send({
-        message: `Host with id ${id} has been successfully deleted!`,
-        host,
+router.put(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const {
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture,
+        aboutMe,
+      } = req.body;
+      const host = await updateHostById(id, {
+        username,
+        password,
+        name,
+        email,
+        phoneNumber,
+        profilePicture,
+        aboutMe,
       });
-    } else {
-      res
-        .status(404)
-        .json({ message: `Host with  id ${id} has not been found!` });
+
+      if (host) {
+        res
+          .status(200)
+          .send({ message: `Host with id ${id} has been updated`, host });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Host with id ${id} has not been found` });
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+  notFoundErrorHandler
+);
+
+router.delete(
+  "/:id",
+  auth,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const host = await deleteHostById(id);
+      if (host) {
+        res.status(200).send({
+          message: `Host with id ${id} has been successfully deleted!`,
+          host,
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: `Host with  id ${id} has not been found!` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 
 export default router;
