e229ac2eb3b85ad6eea75938f7acf0ec65f6f89f
Marco <marco.dmail96@gmail.com>
2025-02-16 21:51:54 +0100

    Update schema

---
+++ prisma/migrations/20250216203114_update_schema/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - Made the column `rating` on table `Property` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Property" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "pricePerNight" REAL NOT NULL,
+    "bedroomCount" INTEGER NOT NULL,
+    "bathRoomCount" INTEGER NOT NULL,
+    "maxGuestCount" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "hostId" TEXT NOT NULL
+);
+INSERT INTO "new_Property" ("bathRoomCount", "bedroomCount", "description", "hostId", "id", "location", "maxGuestCount", "pricePerNight", "rating", "title") SELECT "bathRoomCount", "bedroomCount", "description", "hostId", "id", "location", "maxGuestCount", "pricePerNight", "rating", "title" FROM "Property";
+DROP TABLE "Property";
+ALTER TABLE "new_Property" RENAME TO "Property";
+CREATE INDEX "Property_hostId_idx" ON "Property"("hostId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
