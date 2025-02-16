860021deef7701a9a478f95c8026527cd340afde
Marco <marco.dmail96@gmail.com>
2025-02-16 22:53:14 +0100

    Update deleteAmenityById.js

--- src/services/amenities/deleteAmenityById.js
+++ src/services/amenities/deleteAmenityById.js
@@ -3,7 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const deleteAmenityById = async (id) => {
   const prisma = new PrismaClient();
 
-  const amenity = await prisma.amenity.deleteMany({
+  // Verify if amenity exists
+  const amenity = await prisma.amenity.findUnique({
+    where: { id },
+  });
+
+  if (!amenity) {
+    // If it doesn't exist return null
+    return null;
+  }
+
+  await prisma.amenity.delete({
     where: { id },
   });
   return amenity ? id : null;
